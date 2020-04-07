82afca1fc1aff00546a64ccdc278f869c88014c4
Nicolas JM <42520564+njm181@users.noreply.github.com>
2020-04-06 22:19:04 -0300

    ws updated

--- webServicesGM/select_logueo_user.php
+++ webServicesGM/select_logueo_user.php
@@ -1,19 +1,43 @@
 <?php
 
-include('conexion.php');
-$usuario = $_POST['username'];
-$password = $_POST['password'];
+$hostname_localhost = 'localhost';
+$database_localhost = 'gabriela_mistral';
+$username_localhost = 'root';
+$password_localhost = '';
 
-$consulta = "select * from usuario where username = '$usuario' and password = '$password'";
 
-$resultado = $conexion ->query($consulta);
+$json=array();
+	if(isset($_POST["username"])&&isset($_POST["password"])){
+		$username=$_POST["username"];
+		$password=$_POST["password"];
+				
+		$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 
-while($fila = $resultado->fetch_array()){
-    $evento[] = array_map('utf8_encode', $fila);
+		$consulta="select username, email from usuario where username= '{$username}' and password= '{$password}'";
+		$resultado=mysqli_query($conexion,$consulta);
+			
+		if($registro=mysqli_fetch_array($resultado)){
+			$json['usuario'][]=$registro;
+		//	echo $registro['id'].' - '.$registro['nombre'].' - '.$registro['profesion'].'<br/>';
+		}else{
+			$resultar["username"]='no registra';
+			$resultar["password"]='no registra';
+			$resultar["email"]='no registra';
+			$json['usuario'][]=$resultar;
+		}
+		
+		mysqli_close($conexion);
+		echo json_encode($json);
+	}
+	else{
+		$resultar["success"]=0;
+		$resultar["message"]='Ws no Retorna';
+		$json['usuario'][]=$resultar;
+		echo json_encode($json);
+	}
 
-}
 
-echo json_encode($evento);
-$resultado-> close();
+
+
 
 ?>
